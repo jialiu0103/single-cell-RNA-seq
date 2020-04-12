0df3542c1eeccb115347f8cbcaa92fa2ed188c58
Jia Liu <60714219+jialiu0103@users.noreply.github.com>
2020-04-12 19:16:29 -0400

    Update explore_seurat.R

--- explore_seurat.R
+++ explore_seurat.R
@@ -21,12 +21,46 @@ PlotClusterTree(cells_tree)
 cells_barcode<-CalculateBarcodeInflections(cells_tree)
 SubsetByBarcodeInflections(cells_barcode)
 
-#general look at tsne and umap
-rdc_cells <- RunTSNE(cells, dims = 1:10)
-DimPlot(rdc_cells, reduction = "umap",label = TRUE)
-plot2<-DimPlot(rdc_cells, reduction = "tsne",label = TRUE)
-CombinePlots(plots = list(plot1, plot2),legend="bottom")
+#general look at umap
+DimPlot(cells, reduction = "umap",label = TRUE)
 
 ###############
 #Identify marker genes for each cluster
 ###############
+# Finding differentially expressed features (cluster biomarkers) 
+
+# find all markers of cluster 1
+cluster1.markers <- FindMarkers(cells, ident.1 = 1, min.pct = 0.25)
+head(cluster1.markers, n = 5)
+
+#all clusters
+cells.markers <- FindAllMarkers(cells, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+top2markers=cells.markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
+#sst for delta, ppy for gamma, ghrl for epsilon, cpa1 for acinar, rgs5 for quiescent, pdgfra for activated,
+#vwf for endothelial, sds for macrophage, tpsab1 for mast, trac for cytotoxic, sox10 for schwann
+
+
+##############
+#visualization
+##############
+cells_tsne <- RunTSNE(cells, dims = 1:10)
+head(cells_tsne@reductions$tsne@cell.embeddings)
+DimPlot(cells_tsne, reduction = "tsne",label = TRUE)
+
+#visualize top2 marker gene
+FeaturePlot(cells, features = top2markers$gene[1:4],min.cutoff = 0, max.cutoff = 4)
+FeaturePlot(cells, features = top2markers$gene[5:8],min.cutoff = 0, max.cutoff = 4)
+
+ 
+#Assigning cell type identity to clusters
+new.cluster.ids <- c("GCG", "INS", "SST", "PPY", "GHRL", "CPA1", 
+                     "KRT19", "RGS5", "PDGFRA", 'VWF', 'SDS', 'TPSAB1', 'TRAC', 'SOX10')
+names(new.cluster.ids) <- levels(cells)
+cells <- RenameIdents(cells, new.cluster.ids)
+DimPlot(cells, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+#plot2<-DimPlot(cells, reduction = "tsne", label = TRUE, pt.size = 0.5) + NoLegend()
+
+#bp=DimPlot(cells, reduction = "umap", label = TRUE, pt.size = 0.5)
+#bp + legend(name="cell types",labels=c("Alpha", "Beta", "Delta",'Gamma','Epsilon',
+#                                  'Acinar','Ductal','Quiescent','Activated','Endothelial',
+#                                 'Macrophage','Mast','Cytotoxic','Schwann'))
