656007bd3e5e651ad98864bbac907230a3469990
Jia Liu <60714219+jialiu0103@users.noreply.github.com>
2020-04-22 17:43:15 -0400

    Update explore_seurat.R

--- explore_seurat.R
+++ explore_seurat.R
@@ -70,3 +70,97 @@ DimPlot(cells, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
 library(dplyr)
 top1 <- cells.markers %>% group_by(cluster) %>% top_n(n = 1, wt=avg_logFC)
 DoHeatmap(cells, features = top1$gene)
+
+
+# find novel marker genes
+# Find differentially expressed features between feature0 and all other cells, only
+# search for positive markers
+# Pre-filter features that are detected at <50% frequency in either feature0 and others
+feature0.markers <- FindMarkers(cells, ident.1 = "0", ident.2 = NULL, only.pos = TRUE)
+feature0=feature0.markers[which(feature0.markers$p_val_adj<0.05),]
+#find marker genes already exists
+marker0=AllMarkers[which(AllMarkers$cluster==0),]
+marker0=marker0$gene
+#drop marker genes already exist
+f0=feature0[-which(row.names(feature0) %in% marker0),]
+
+feature1.markers <- FindMarkers(cells, ident.1 = "1", ident.2 = NULL, only.pos = TRUE)
+feature1=feature1.markers[which(feature1.markers$p_val_adj<0.05),]
+marker1=AllMarkers[which(AllMarkers$cluster==1),]
+marker1=marker1$gene
+f1=feature1[-which(row.names(feature1) %in% marker1),]
+
+feature2.markers <- FindMarkers(cells, ident.1 = "2", ident.2 = NULL, only.pos = TRUE)
+feature2=feature2.markers[which(feature2.markers$p_val_adj<0.05),]
+marker2=AllMarkers[which(AllMarkers$cluster==2),]
+marker2=marker2$gene
+f2=feature2[-which(row.names(feature2) %in% marker2),]
+
+feature3.markers <- FindMarkers(cells, ident.1 = "3", ident.2 = NULL, only.pos = TRUE)
+feature3=feature3.markers[which(feature3.markers$p_val_adj<0.05),]
+marker3=AllMarkers[which(AllMarkers$cluster==3),]
+f3=feature3[-which(row.names(feature3) %in% marker3$gene),]
+
+feature4.markers <- FindMarkers(cells, ident.1 = "4", ident.2 = NULL, only.pos = TRUE)
+feature4=feature4.markers[which(feature4.markers$p_val_adj<0.05),]
+marker4=AllMarkers[which(AllMarkers$cluster==4),]
+f4=feature4[-which(row.names(feature4) %in% marker4$gene),]
+
+feature5.markers <- FindMarkers(cells, ident.1 = "5", ident.2 = NULL, only.pos = TRUE)
+feature5=feature5.markers[which(feature5.markers$p_val_adj<0.05),]
+marker5=AllMarkers[which(AllMarkers$cluster==5),]
+f5=feature5[-which(row.names(feature5) %in% marker5$gene),]
+
+feature6.markers <- FindMarkers(cells, ident.1 = "6", ident.2 = NULL, only.pos = TRUE)
+feature6=feature6.markers[which(feature6.markers$p_val_adj<0.05),]
+marker6=AllMarkers[which(AllMarkers$cluster==6),]
+f6=feature6[-which(row.names(feature6) %in% marker6$gene),]
+
+feature.markers <- FindMarkers(cells, ident.1 = "7", ident.2 = NULL, only.pos = TRUE)
+feature=feature.markers[which(feature.markers$p_val_adj<0.05),]
+marker=AllMarkers[which(AllMarkers$cluster==7),]
+f7=feature[-which(row.names(feature) %in% marker$gene),]
+
+feature.markers <- FindMarkers(cells, ident.1 = "8", ident.2 = NULL, only.pos = TRUE)
+feature=feature.markers[which(feature.markers$p_val_adj<0.05),]
+marker=AllMarkers[which(AllMarkers$cluster==8),]
+f8=feature[-which(row.names(feature) %in% marker$gene),]
+
+feature.markers <- FindMarkers(cells, ident.1 = "9", ident.2 = NULL, only.pos = TRUE)
+feature=feature.markers[which(feature.markers$p_val_adj<0.05),]
+marker=AllMarkers[which(AllMarkers$cluster==9),]
+f9=feature[-which(row.names(feature) %in% marker$gene),]
+
+feature.markers <- FindMarkers(cells, ident.1 = "10", ident.2 = NULL, only.pos = TRUE)
+feature=feature.markers[which(feature.markers$p_val_adj<0.05),]
+marker=AllMarkers[which(AllMarkers$cluster==10),]
+f10=feature[-which(row.names(feature) %in% marker$gene),]
+
+feature.markers <- FindMarkers(cells, ident.1 = "11", ident.2 = NULL, only.pos = TRUE)
+feature=feature.markers[which(feature.markers$p_val_adj<0.05),]
+marker=AllMarkers[which(AllMarkers$cluster==11),]
+f11=feature[-which(row.names(feature) %in% marker$gene),]
+
+feature.markers <- FindMarkers(cells, ident.1 = "12", ident.2 = NULL, only.pos = TRUE)
+feature=feature.markers[which(feature.markers$p_val_adj<0.05),]
+marker=AllMarkers[which(AllMarkers$cluster==12),]
+f12=feature[-which(row.names(feature) %in% marker$gene),]
+
+
+f0$cluster=rep(0,length(f0$p_val))
+f1$cluster=rep(1,length(f1$p_val))
+f2$cluster=rep(2,length(f2$p_val))
+f3$cluster=rep(3,length(f3$p_val))
+f4$cluster=rep(4,length(f4$p_val))
+f5$cluster=rep(5,length(f5$p_val))
+f6$cluster=rep(6,length(f6$p_val))
+f7$cluster=rep(7,length(f7$p_val))
+f8$cluster=rep(8,length(f8$p_val))
+f9$cluster=rep(9,length(f9$p_val))
+f10$cluster=rep(10,length(f10$p_val))
+f11$cluster=rep(11,length(f11$p_val))
+f12$cluster=rep(12,length(f12$p_val))
+
+novelmarker=rbind(f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12)
+write.csv(novelmarker,'novelmarker.csv')
+
